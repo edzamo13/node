aae646b5913da03001f11c7f8987df7f0bc28535
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-07-03 19:34:53 -0500

    add folder nestJs module

---
+++ nestJS/platzi-store-modular/src/app.module.ts
@@ -0,0 +1,36 @@
+import { HttpModule, HttpService, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+//https://docs.nestjs.com/techniques/http-module
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
+})
+export class AppModule {}
