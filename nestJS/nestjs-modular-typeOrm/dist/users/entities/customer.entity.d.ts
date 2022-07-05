2202f14bee84d38389bd7db4898bc86ed3916602
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-07-05 09:28:26 -0500

    add database

--- nestJS/nestjs-modular-typeOrm/dist/users/entities/customer.entity.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Customer {
-    id: number;
-    name: string;
-    lastName: string;
-    phone: string;
-}
--- nestJS/nestjs-modular-typeOrm/dist/users/entities/order.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './user.entity';
-import { Product } from './../../products/entities/product.entity';
-export declare class Order {
-    date: Date;
-    user: User;
-    products: Product[];
-}
